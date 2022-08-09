279d15b6b4670f229e044dd913c4d0fb8f2a5e39
Bradley <bswj90@gmail.com>
2022-08-09 19:45:36 -0400

    fixed github merge conflicts

--- controllers/index.js
+++ controllers/index.js
@@ -9,10 +9,4 @@ router.use((req, res) => {
   res.status(404).end();
 });
 
-// app.use((req, res, next) => {
-//   res.status(200).json({
-//     message: 'It works'
-//   });
-// });
-
 module.exports = router;
