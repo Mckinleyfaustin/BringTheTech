ed13cc441e182bec5a6078b7482b08bf32420597
Bradley <bswj90@gmail.com>
2022-08-09 19:16:43 -0400

    added in rendering with handlebars

--- controllers/htmlRoutes/index.js
+++ controllers/htmlRoutes/index.js
@@ -1,9 +1,20 @@
 const router = require("express").Router();
 const path = require("path");
+const sequelize = require("../../config/connection");
 const { Inventory } = require("../../models");
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/index.html"));
+  Inventory.findAll({
+    attributes: ["id", "title", "discription", "price", "image_url"],
+  })
+    .then((dbInventoryData) => {
+      const items = dbInventoryData.map((item) => item.get({ plain: true }));
+      res.render("product-body", { items });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -9,16 +9,3 @@ getInventory = async () => {
   var data = await dbData.json();
   return data;
 };
-
-getInventory().then((dbInventoryData) => {
-  const ulEl = document.createElement("ul");
-  for (let i = 0; i < dbInventoryData.length; i++) {
-    const liEl = document.createElement("li");
-    liEl.innerText = dbInventoryData[i].title;
-    ulEl.appendChild(liEl);
-    const emmaAge = document.createElement("li");
-    emmaAge.innerText = dbInventoryData[i].price;
-    ulEl.appendChild(emmaAge);
-    document.body.appendChild(ulEl);
-  }
-});
